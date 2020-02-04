e0c5ad97f84ffe77ac2de16f3af9b8c81410468f
Moudi Kawi <kawimoudi@gmail.com>
2020-02-04 18:12:05 +0000

    Add Asabeneh JavaScript Day 1 Challenge

---
+++ Asabeneh/JavaScript/Day 1/datatypes.js
@@ -0,0 +1,35 @@
+let varOne, varTwo, varThree, varFour;
+
+let varFive = 10;
+let varSix = 'Hello';
+let varSeven = false;
+let varEight = [ 10, 20, 'thirty', 40, true ];
+
+let firstName = 'Moudi';
+let lastName = 'Kawi';
+let maritalStatus = false;
+let country = 'United Kingdom';
+let age = 23;
+
+let firstName = 'Moudi',
+	lastName = 'Kawi',
+	maritalStatus = false,
+	country = 'United Kingdom',
+	age = 23;
+
+let myAge = 23,
+	yourAge = 25;
+
+console.log(myAge);
+console.log(yourAge);
+
+// typeof operator
+
+console.log(typeof varOne);
+console.log(typeof varTwo);
+console.log(typeof varThree);
+console.log(typeof varFour);
+console.log(typeof varFive);
+console.log(typeof varSix);
+console.log(typeof varSeven);
+console.log(typeof varEight);
---
+++ Asabeneh/JavaScript/Day 1/variable.js
@@ -0,0 +1,14 @@
+let str = 'This is a string';
+let num = 50;
+let bool = true;
+let notDefined;
+let empty = null;
+
+/*
+    ES5
+    var     Old method of declaring variable
+
+    ES6
+    let     Variables can be reassigned
+    const   Variables cannot be reassigned
+*/
