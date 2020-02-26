b4c0b8aafe1f182a4a20ee9e26f42fb23be9ea31
Moudi Kawi <kawimoudi@gmail.com>
2020-02-26 20:19:01 +0100

    Add solutions to Asabeneh JavaScript day 16 exercises

---
+++ Asabeneh/JavaScript/Day 16/Exercise Level 1.js
@@ -0,0 +1,17 @@
+const skills = [ 'HTML', 'CSS', 'JS', 'React', 'Node', 'Python' ];
+
+let age = 250;
+let isMarried = true;
+
+const student = {
+	firstName: 'Asabeneh',
+	lastName: 'Yetayehe',
+	age: 250,
+	isMarried: true,
+	skills: [ 'HTML', 'CSS', 'JS', 'React', 'Node', 'Python' ]
+};
+
+const skillsJSON = JSON.stringify(skills);
+const ageJSON = JSON.stringify(age);
+const isMarriedJSON = JSON.stringify(isMarried);
+const studentJSON = JSON.stringify(student);
---
+++ Asabeneh/JavaScript/Day 16/Exercise Level 2.js
@@ -0,0 +1,9 @@
+const student = {
+	firstName: 'Asabeneh',
+	lastName: 'Yetayehe',
+	age: 250,
+	isMarried: true,
+	skills: [ 'HTML', 'CSS', 'JS', 'React', 'Node', 'Python' ]
+};
+
+const studentJSON = JSON.stringify(student, [ 'firstName', 'lastName', 'skills' ]);
